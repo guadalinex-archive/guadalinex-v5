25362cd71ab5bc5d3fae858f27e78ac9f4126b58
dteyssiere <dteyssiere@fecba971-2e3e-0410-8991-b5285d0e4d01>
2008-06-25 07:57:45 +0000

    updateing version

--- apps/gedit-plugin-tloleo/trunk/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gedit-plugin-tloleo package
-#
-0 4	* * *	root	gedit-plugin-tloleo_maintenance
--- apps/gedit-plugin-tloleo/trunk/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gedit-plugin-tloleo):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="gedit-plugin-tloleo" command="/usr/bin/gedit-plugin-tloleo"
