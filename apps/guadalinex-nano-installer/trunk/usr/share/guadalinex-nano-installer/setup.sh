53c01210b1b2a9f3648f5ecc1ad8c55c965a1732
rcmorano <rcmorano@fecba971-2e3e-0410-8991-b5285d0e4d01>
2009-01-13 12:03:04 +0000

    added missing features and added missing backtitles

--- apps/guadalinex-nano-installer/trunk/usr/share/guadalinex-nano-installer/setup.sh
+++ apps/guadalinex-nano-installer/trunk/usr/share/guadalinex-nano-installer/setup.sh
@@ -2,15 +2,26 @@
 
 VERSION="V5"
 DIALOG="/usr/bin/dialog"
+INDEXURL="http://www.guadalinex.org/distro/$VERSION/perfiles/index"
 
-# download options file
-#wget http://gensys/$GNICONF -O /etc/gni-options
+# show index download url confirmation
+$DIALOG --aspect 15 --cr-wrap --nocancel --title "Confirmación de la URL" \
+--backtitle "Instalación de Guadalinex Mínima" \
+--trim --inputbox "Por favor, confirme que esta es la URL de indices que quiere \
+usar o modifiquela manualmente:" 0 100 "$URL" 2> /tmp/gni-url
+
+# reset index download url from the inputbox
+INDEXURL=$(cat /tmp/gni-url)
+
+# download index file
 test -f /etc/gni/index && rm /etc/gni/index
-wget http://www.guadalinex.org/distro/$VERSION/perfiles/index -O /etc/gni/index
+wget $INDEXURL -O /etc/gni/index
 
+# show any error if there was any
 if [ $? -ne 0 ]
 then
 	$DIALOG --aspect 15 --cr-wrap --title "Error de conexión" --trim \
+	--backtitle "Instalación de Guadalinex Mínima" \
         --msgbox "No se pudo descargar la configuración del instalador.
 		 La instalación no ha sido completada, se intentará completar
 		 la próxima vez que se inicie el sistema." 0 0
@@ -30,6 +41,7 @@ fi
 if [ $? -eq 69 ]
 then
         $DIALOG --aspect 15 --cr-wrap --title "Error en la instalación" --trim \
+	--backtitle "Instalación de Guadalinex Mínima" \
         --msgbox "Hubo algún problema durante la instalación de paquetes,
 		 revise el fichero '/var/log/gni.log' para obtener más
 		 información." 0 0
